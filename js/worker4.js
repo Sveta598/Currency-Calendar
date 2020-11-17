8ca4deedf25b91cd6202c9583cea41fc7716feec
Svetlana Pavlovskaya <sveta.pavl@tut.by>
2020-11-17 10:47:46 +0300

    version22

---
+++ js/worker4.js
@@ -0,0 +1,16 @@
+function mapper (commits) {
+    const date = commits.map(element => {
+            return element.Date.slice(0, 10);
+    });
+    const rate = commits.map(element => {
+            return element.Cur_OfficialRate;
+    });
+    return {date, rate}
+};
+
+onmessage = function(e) {
+    fetch(e.data)
+        .then(response => response.json())
+        .then(mapper) 
+        .then(self.postMessage);
+}
