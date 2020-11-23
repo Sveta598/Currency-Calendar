08a8f1f4dea2da15e295cb1d8fbc0f060b016afa
Svetlana Pavlovskaya <sveta.pavl@tut.by>
2020-11-23 22:47:14 +0300

    version24.11

--- js/script3.js
+++ js/script3.js
@@ -71,13 +71,14 @@ function getOptions(currencyData) {
 
 const dateFormat = ('YYYY-MM-DD');
 const currentDate = dayjs().format(dateFormat);
-document.querySelector('.navigation__input_first').max = currentDate;
+const currentDateSubtract = dayjs().subtract(1, 'd').format(dateFormat);
+document.querySelector('.navigation__input_first').max = currentDateSubtract;
 document.querySelector('.navigation__input_second').max = currentDate;
 
 function getChart () {
     const cur = valuta.value;
-    const startDate = startDateId.value;
-    const endDate = endDateId.value;
+    let startDate = startDateId.value;
+    let endDate = endDateId.value;
 
     let currencies = localStorage.getItem('currencyObject');
     if (!currencies) {
@@ -87,58 +88,223 @@ function getChart () {
         currencies = JSON.parse(currencies);
     }
 
-    /*console.log(currencies);*/
-
     const curObj = currencies[cur];
-    const urlArray = [];
     const newArray = curObj.payload;
+    const periodArr = [];
+    
     for (let i = 0; i < newArray.length; i++) {
-        const curID = newArray[i].Cur_ID;
-        const curDataStart = newArray[i].Cur_DateStart;
-        const curDataEnd = newArray[i].Cur_DateEnd;
-        const curDataStart2 = newArray[i + 1].Cur_DateStart;
-        const curDataEnd2 = newArray[i + 1].Cur_DateEnd;
-        const curDataEndAdd = dayjs(curDataEnd).add(1, 'd');
-        const comparison1 = dayjs(startDate).isBefore(curDataEndAdd);
-        const comparison2 = dayjs(endDate).isAfter(curDataEnd);
-        const comparison3 = dayjs(curDataStart2).isAfter(curDataEnd);
+    
+        const period = curObj.payload[i];
+        
+        if (startDate > period.Cur_DateEnd.slice(0, 10)) { continue; }
 
-        if (comparison1 === true && comparison2 === true) {
-            url2 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${startDate}T00:00:00&endDate=${curDataEnd}T00:00:00`;
-            urlArray.push(url2);
-            url3 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${curDataStart}T00:00:00&endDate=${endDate}T00:00:00`;
-            urlArray.push(url3)
+        const range = dayjs(endDate).diff(startDate, 'year');
+
+        let endD = '';
+        //если curId не менялся
+        if (curObj.payload.length === 1) {
+            startDate = startDateId.value;
+            endD = endDate;
+        }
+        //если curId менялся
+        else {
+            if (endDate <= period.Cur_DateEnd.slice(0, 10)) {
+                endD = endDate;
+                if (startDate >= curObj.payload[i].Cur_DateStart) {
+                    startDate = startDateId.value;
+                } else {
+                    startDate = curObj.payload[i].Cur_DateStart.slice(0, 10);
+                }
+            } else {
+                 endD = period.Cur_DateEnd.slice(0, 10);
+                 startDate = startDateId.value;
+            }
         }
 
-        if (comparison1 === true && ) {
+        //если длина запрашиваемого периода больше года
+        if (range >= 1) {
+            //если длина менее 2 лет
+            if (range < 2) {
+                if(endD > period.Cur_DateEnd.slice(0, 10)) {
+                    endD = period.Cur_DateEnd.slice(0, 10);
+                }
 
+                endD = dayjs(startDate).add(1, 'y').subtract(1, 'd').format(dateFormat);
+    
+                periodArr.push({
+                    curId: period.Cur_ID,
+                    startDate,
+                    endDate: endD,
+                    
+                });
+
+                if (endD === dayjs(startDate).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                    startDate5 = dayjs(startDate).add(1, 'y').format(dateFormat);
+                    if (endDate > period.Cur_DateEnd.slice(0, 10)) {
+                        endDate = period.Cur_DateEnd.slice(0, 10);
+                    }
+
+                    periodArr.push({
+                        curId: period.Cur_ID,
+                        startDate: startDate5,
+                        endDate: endDate,
+                    });
+                }
+            }
+            //если длина более 2 лет
+            else {
+                if(endD > period.Cur_DateEnd.slice(0, 10)) {
+                    endD = period.Cur_DateEnd.slice(0, 10);
+
+                    //////////////////////////////////////////////////
+                    if (endD = period.Cur_DateEnd.slice(0, 10)) {
+                        range2 = dayjs(endDate).diff(endD, 'year');
+                        curId2 = curObj.payload[i+1].Cur_ID;
+                        for (let n = 0; n < range2; n++) {
+                            startDate5 = dayjs(endD).add(n, 'y').add(1, 'd').format(dateFormat);
+                            endD5 = dayjs(startDate5).add(1, 'y').subtract(1, 'd').format(dateFormat);
+
+                            periodArr.push({
+                                curId: curId2,
+                                startDate: startDate5,
+                                endDate: endD5,
+                            });
+                        }
+                    }
+                    /*****************************************************/
+                    if (endD5 = dayjs(startDate5).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                        curId2 = curObj.payload[i+1].Cur_ID;
+                        startDate51 = dayjs(endD5).add(1, 'd').format(dateFormat);
+
+                        periodArr.push({
+                            curId: curId2,
+                            startDate: startDate51,
+                            endDate: endDate,
+                        });
+                    }
+                    ////////////////////////////////////////////////// 
+                }
+
+                endD = dayjs(startDate).add(1, 'y').subtract(1, 'd').format(dateFormat);
+        
+                periodArr.push({
+                    curId: period.Cur_ID,
+                    startDate,
+                    endDate: endD,
+                    
+                });
+
+                if (endD === dayjs(startDate).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                    for (let k = 1; k < range; k++) {
+                        startDate2 = dayjs(startDate).add(k, 'y').format(dateFormat);
+                        endD2 = dayjs(startDate2).add(1, 'y').subtract(1, 'd').format(dateFormat);
+
+                        if(endD2 > period.Cur_DateEnd.slice(0, 10)) {
+                            endD2 = period.Cur_DateEnd.slice(0, 10);
+                                    //////////////////////////////////////////////////
+                            if (endD2 = period.Cur_DateEnd.slice(0, 10)) {
+                                range3 = dayjs(endDate).diff(endD2, 'year');
+                                curId2 = curObj.payload[i+1].Cur_ID;
+                                for (let n = 0; n < range2; n++) {
+                                    startDate6 = dayjs(endD2).add(n, 'y').add(1, 'd').format(dateFormat);
+                                    endD6 = dayjs(startDate6).add(1, 'y').subtract(1, 'd').format(dateFormat);
+
+                                    periodArr.push({
+                                        curId: curId2,
+                                        startDate: startDate6,
+                                        endDate: endD6,
+                                    });
+                                }
+                            }
+                            /*****************************************************/
+                            if (endD6 = dayjs(startDate6).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                                curId2 = curObj.payload[i+1].Cur_ID;
+                                startDate61 = dayjs(endD6).add(1, 'd').format(dateFormat);
+
+                                periodArr.push({
+                                    curId: curId2,
+                                    startDate: startDate61,
+                                    endDate: endDate,
+                                });
+                            }
+                            ////////////////////////////////////////////////// 
+                        }
+
+                        periodArr.push({
+                            curId: period.Cur_ID,
+                            startDate: startDate2,
+                            endDate: endD2,
+                        });
+                    }
+                }
+                
+                if (endD2 = dayjs(startDate2).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                    startDate3 = dayjs(endD2).add(1, 'd').format(dateFormat);
+
+                    if(endDate > period.Cur_DateEnd.slice(0, 10)) {
+                        endDate = period.Cur_DateEnd.slice(0, 10);
+
+                              //////////////////////////////////////////////////
+                              if (endDate = period.Cur_DateEnd.slice(0, 10)) {
+                                endDate111 =  endDateId.value;
+                                range4 = dayjs(endDate111).diff(endDate, 'year');
+                                curId2 = curObj.payload[i+1].Cur_ID;
+                                for (let n = 0; n < range4; n++) {
+                                    startDate7 = dayjs(endDate).add(n, 'y').add(1, 'd').format(dateFormat);
+                                    endD7 = dayjs(startDate7).add(1, 'y').subtract(1, 'd').format(dateFormat);
+
+                                    periodArr.push({
+                                        curId: curId2,
+                                        startDate: startDate7,
+                                        endDate: endD7,
+                                    });
+                                }
+                            }
+                            /*****************************************************/
+                            if (endD7 = dayjs(startDate7).add(1, 'y').subtract(1, 'd').format(dateFormat)) {
+                                curId2 = curObj.payload[i+1].Cur_ID;
+                                startDate71 = dayjs(endD7).add(1, 'd').format(dateFormat);
+
+                                periodArr.push({
+                                    curId: curId2,
+                                    startDate: startDate71,
+                                    endDate: endDate111,
+                                });
+                            }
+                            ////////////////////////////////////////////////// 
+                    }
+                    
+                    periodArr.push({
+                        curId: period.Cur_ID,
+                        startDate: startDate3,
+                        endDate: endDate,
+                    
+                    });
+                }
+            }
+        }
+        //если длина запрашиваемого периода меньше года
+        else {
+            periodArr.push({
+                curId: period.Cur_ID,
+                startDate,
+                endDate: endD,
+                    
+            });
         }
 
-
-        const url1 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${startDate}T00:00:00&endDate=${endDate}T00:00:00`;
-        urlArray.push(url1);
-        const url2 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${startDate}T00:00:00&endDate=${curDataEnd}T00:00:00`;
-        urlArray.push(url2);
-        const url3 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${curDataStart}T00:00:00&endDate=${endDate}T00:00:00`;
-        urlArray.push(url3)
-        const url4 = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${curID}?startDate=${curDataStart}T00:00:00&endDate=${curDataEnd}T00:00:00`;
-        urlArray.push(url4);
-
+       if (endDate <= period.Cur_DateEnd.slice(0, 10)) { break; }
        
-        worker1.postMessage(urlArray);
     }
-
-   
     
+    console.log(periodArr);
     
-    
-
     /*
     * 1. разделить по промежуткам связанным с id
     * 2. разделить по промежуткам связанным с ограничением на период в 1 год
     * */
 
-    /*worker1.postMessage({cur, startDate, endDate});*/
+   worker1.postMessage(periodArr);
 }
 
 function chart(categories, data) {
